785cc86ef98d9bd6925622beb71710f8d4dc5b3e
hushangd <hushangid@gmail.com>
2025-06-21 11:09:04 +0500

    feat: Update filters and header components for improved styling and functionality

--- app/about/team/page.tsx
+++ app/about/team/page.tsx
@@ -2,7 +2,7 @@ import MeetTheTeam from '@/ui-components/team/team';
 
 export default function Team() {
   return (
-    <div className="md:pt-[69px] pb-14 md:pb-[116px]">
+    <div className="pb-14 md:pb-[116px]">
       <MeetTheTeam />
     </div>
   );
