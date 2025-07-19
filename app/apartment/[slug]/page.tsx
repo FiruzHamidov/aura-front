01d9e0974de086e491d7071693ae28149605da44
hushangd <hushangid@gmail.com>
2025-07-19 16:55:13 +0500

    refactor: improve code readability and structure in ApartmentPage component

--- app/apartment/[slug]/page.tsx
+++ app/apartment/[slug]/page.tsx
@@ -1,29 +1,30 @@
 import { Property, PropertyPhoto } from '@/services/properties/types';
 import GalleryWrapper from './_components/GalleryWrapper';
 
+type PageProps = Promise<{
+  slug: string;
+}>;
+
 async function getApartment(id: string): Promise<Property> {
-    const res = await fetch(`https://backend.aura.tj/api/properties/${id}`, {
-        cache: 'no-store',
-    });
+  const res = await fetch(`https://backend.aura.tj/api/properties/${id}`, {
+    cache: 'no-store',
+  });
 
-    if (!res.ok) {
-        throw new Error('Ошибка при получении объекта');
-    }
+  if (!res.ok) {
+    throw new Error('Ошибка при получении объекта');
+  }
 
-    return res.json();
+  return res.json();
 }
 
-export default async function ApartmentPage({ params }: { params: { slug: string } }) {
-    const apartment = await getApartment(params.slug);
+export default async function ApartmentPage({ params }: { params: PageProps }) {
+  const { slug } = await params;
+  const apartment = await getApartment(slug);
 
-    const photos: string[] = apartment.photos?.map(
-        (p: PropertyPhoto) => `https://backend.aura.tj/storage/${p.file_path}`
+  const photos: string[] =
+    apartment.photos?.map(
+      (p: PropertyPhoto) => `https://backend.aura.tj/storage/${p.file_path}`
     ) ?? [];
 
-    return (
-        <GalleryWrapper
-            apartment={apartment}
-            photos={photos}
-        />
-    );
+  return <GalleryWrapper apartment={apartment} photos={photos} />;
 }
