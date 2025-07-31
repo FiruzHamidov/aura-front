03dc23c54518f5ff12c2fc8bd4d4e91535a796a0
FH <homidovfiruz23@gmail.com>
2025-07-31 10:49:25 +0500

    fix update

--- app/_components/banner.tsx
+++ app/_components/banner.tsx
@@ -37,7 +37,7 @@ export const MainBanner: FC<{ title: string }> = ({ title }) => {
   const handleSearch = () => {
     const searchParams = new URLSearchParams();
 
-    if (propertyType) searchParams.append('propertyType', propertyType);
+    if (propertyType) searchParams.append('type_id', propertyType);
     if (roomsFrom) searchParams.append('roomsFrom', roomsFrom);
     if (roomsTo) searchParams.append('roomsTo', roomsTo);
     if (priceFrom) searchParams.append('priceFrom', priceFrom);
