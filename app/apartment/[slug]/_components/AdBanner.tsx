a7d1d39691608b6e3673fce2ad5d0951dd881f70
FH <homidovfiruz23@gmail.com>
2025-11-05 11:57:44 +0500

    test adsense

--- app/apartment/[slug]/_components/AdBanner.tsx
+++ app/apartment/[slug]/_components/AdBanner.tsx
@@ -1,25 +1,25 @@
-import { useEffect } from 'react';
-
-const AdBanner = (props) => {
-    useEffect(() => {
-        try {
-            (window.adsbygoogle = window.adsbygoogle || []).push({});
-        } catch (err) {
-            console.log(err);
-        }
-    }, []);
-
-    return (
-        <ins
-            className="adsbygoogle adbanner-customize max-w-[375px]"
-            style={{
-                display: 'block',
-                overflow: 'hidden',
-                width: 'auto'
-            }}
-            data-ad-client={process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}
-            {...props}
-        />
-    );
-};
-export default AdBanner;
+// import { useEffect } from 'react';
+//
+// const AdBanner = (props) => {
+//     useEffect(() => {
+//         try {
+//             (window.adsbygoogle = window.adsbygoogle || []).push({});
+//         } catch (err) {
+//             console.log(err);
+//         }
+//     }, []);
+//
+//     return (
+//         <ins
+//             className="adsbygoogle adbanner-customize max-w-[375px]"
+//             style={{
+//                 display: 'block',
+//                 overflow: 'hidden',
+//                 width: 'auto'
+//             }}
+//             data-ad-client={process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}
+//             {...props}
+//         />
+//     );
+// };
+// export default AdBanner;
