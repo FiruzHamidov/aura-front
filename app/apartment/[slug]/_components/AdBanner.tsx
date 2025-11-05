e75680395c54a6b02799560378918a46f9cb9b61
FH <homidovfiruz23@gmail.com>
2025-11-05 14:34:23 +0500

    test adsense

--- app/apartment/[slug]/_components/AdBanner.tsx
+++ app/apartment/[slug]/_components/AdBanner.tsx
@@ -52,6 +52,7 @@ const AdBanner = (props: AdsBannerProps) => {
                 display: "block",
                 overflow: "hidden",
                 border: process.env.NODE_ENV === "development" ? "1px solid red" : "none",
+                minWidth: "250px"
             }}
             data-adtest="on"
             data-ad-client={process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}
