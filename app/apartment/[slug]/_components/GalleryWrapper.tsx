b8fba4410191219a001430870a64ab133ac0ec4b
FH <homidovfiruz23@gmail.com>
2025-11-05 16:30:52 +0500

    test adsense

--- app/apartment/[slug]/_components/GalleryWrapper.tsx
+++ app/apartment/[slug]/_components/GalleryWrapper.tsx
@@ -787,7 +787,7 @@ export default function GalleryWrapper({apartment, photos}: Props) {
                         data-ad-slot="5694010534"
                         data-full-width-responsive="true"
                         data-ad-layout="in-article"
-                        data-ad-format="fluid"
+                        data-ad-format="auto"
                     />
                 </div>
             </div>
